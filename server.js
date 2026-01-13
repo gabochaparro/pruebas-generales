d7937f78d6e55011b1946cf6c331dcf695f51ecf
gabochaparro <143703358+gabochaparro@users.noreply.github.com>
2026-01-13 03:46:41 -0500

    Remove upgrade function from WebSocket setup
    Removed the upgrade function from WebSocket configuration.

--- server.js
+++ server.js
@@ -5,23 +5,9 @@ const app = uWS.App();
 const PORT = process.env.PORT || 80;
 
 app.ws('/*', {
-  upgrade: (res, req, ctx) => {
-    res.upgrade(
-      {
-        ip: Buffer.from(res.getRemoteAddress()).join('.'),
-        token: req.getHeader('authorization'),
-        ua: req.getHeader('user-agent')
-      },
-      req.getHeader('sec-websocket-key'),
-      req.getHeader('sec-websocket-protocol'),
-      req.getHeader('sec-websocket-extensions'),
-      ctx
-    );
-  },
 
   open: (ws) => {
     ws.subscribe('bot');
-    console.log(`Conectado el cliente: ${ws.ip}`);
   },
 
   message: (ws, message) => {
